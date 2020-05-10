11f2f88abab85dbb9c4e3550b65338794491f66d
sweetechka <titvs1337@mail.ru>
2020-05-10 16:46:42 +0700

    Changed makefile

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "Logo.hpp"
+#include "Rules.hpp"
 #include <iostream>
 #include <stdlib.h>
 
 using namespace std;
 int main() {
+	setlocale(LC_ALL, "Russian");
 	Logo();
+	Rules();
 	system("pause");
 	return 0;
 }
