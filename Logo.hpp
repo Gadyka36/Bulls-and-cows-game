2586bbd8f019c34404cf2c2d725dc6d26d33b586
sweetechka <titvs1337@mail.ru>
2020-05-10 15:47:38 +0700

    Fixed merge conflicts

--- Logo.hpp
+++ Logo.hpp
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 #pragma once
 using namespace std;
-||||||| a99bd9b
-#ifndef Logo
-#define Logo
-=======
-#pragma once
->>>>>>> 7d82e5b4a1702d6cf5c71de9455682227af6e3b1
 void Logo();
 
--- main.cpp
+++ main.cpp
@@ -5,12 +5,6 @@
 using namespace std;
 int main() {
 	Logo();
-<<<<<<< HEAD
-||||||| a99bd9b
-	return 0;
-=======
-	
->>>>>>> 7d82e5b4a1702d6cf5c71de9455682227af6e3b1
 	system("pause");
 	return 0;
 }
